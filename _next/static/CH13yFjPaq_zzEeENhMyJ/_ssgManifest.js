4b227fb36d6fbcaa69943aa131e386331253a9de
matija8 <matijanme@gmail.com>
2022-01-29 15:16:36 +0100

    Updates

--- _next/static/CH13yFjPaq_zzEeENhMyJ/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Foblasti-rada\u002F[workAreaId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/nas-tim":["static\u002Fchunks\u002Fpages\u002Fnas-tim.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fnas-tim"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
