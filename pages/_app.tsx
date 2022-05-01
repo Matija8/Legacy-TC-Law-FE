ec82627e6887f861a32f92b97bcc9f1ba05cb4cf
matija8 <matijanme@gmail.com>
2022-05-02 01:27:56 +0200

    chore: Update types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import '../styles/globals.scss';
-// import type { AppProps } from 'next/app';
-// TODO: AppProps
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: /* AppProps */ any) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
