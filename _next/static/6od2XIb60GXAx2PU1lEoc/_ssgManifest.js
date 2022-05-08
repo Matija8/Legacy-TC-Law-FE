11e8e495c584a365874749af4e4ee0398e5bc666
matija8 <matijanme@gmail.com>
2022-05-08 14:14:30 +0200

    Updates

---
+++ _next/static/6od2XIb60GXAx2PU1lEoc/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fnovosti","\u002Foblasti-rada\u002F[workDomainId]","\u002Fnas-tim\u002F[employeePagePath]","\u002Fnovosti\u002F[newsArticleId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
