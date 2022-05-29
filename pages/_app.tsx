522a1ef7e37ca9d3deb005b1aa324d544232efc2
matija8 <matijanme@gmail.com>
2022-05-29 11:02:15 +0200

    fix: Switch from cdn to npm for font imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
+// https://stackoverflow.com/questions/68480061/which-one-font-awesome-in-react-app-using-cdn-or-npm
+import '@fontsource/montserrat';
 import '../styles/globals.scss';
+
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         {/* https://fonts.google.com/specimen/Montserrat#standard-styles */}
         {/* https://nextjs.org/docs/messages/google-font-display */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -15,7 +15,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat&display=optional"
           rel="stylesheet"
-        />
+        /> */}
       </Head>
       <body>
         <Main />
