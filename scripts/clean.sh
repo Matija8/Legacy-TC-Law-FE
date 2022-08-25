6c8b25f6b1971c2e056c6f019b85774a75c3d9af
matija8 <matijanme@gmail.com>
2022-08-25 12:25:27 +0200

    feat: move *clean* to bash script

---
+++ scripts/clean.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# coding: UTF-8
+
+script_dir=$(realpath $(dirname "$0"))
+
+function main {
+    printf "Starting clean operation...\n\n"
+    cd "$script_dir/.."
+    # pwd
+
+    rm -rf .next
+    echo "rm /.next dir done."
+
+    rm -rf out
+    echo "rm /out dir done."
+
+    rm -f tc-law*.zip
+    echo "rm *.zip files done."
+
+    rm -rf node_modules
+    echo "rm node_modules done."
+
+    # rm -f yarn.lock && rm -f package-lock.json
+    # echo "rm lockfiles done."
+
+    printf "\nCleaning done!\n"
+}
+
+main
